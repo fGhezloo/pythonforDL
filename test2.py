2a45415cee3bc0edc38bc4da0b300167aa4f8a72
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-08-01 23:39:40 +0430

    colored image

--- test2.py
+++ test2.py
@@ -1,14 +1,21 @@
-import cv2
+import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 #draw line on image
-img = cv2.imread('/Users/apple/Desktop/a.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv.imread('/Users/apple/Desktop/a.jpg',cv.IMREAD_GRAYSCALE)
 
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.plot([200,300,400],[100,200,300],'c', linewidth=5)
 plt.show()
-cv2.imwrite('watchgray.png',img)
+cv.imwrite('watchgray.png',img)
 
+
+#load colored image
+
+image=cv.imread('/Users/apple/Desktop/a.jpg', cv.IMREAD_COLOR)#Load the image
+
+cv.imshow('image', image) #Show the image
+k = cv.waitKey(0)
